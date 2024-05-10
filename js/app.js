d812d6b08aeefb250d44c799ef7d772046b482e1
Yatziel Ilhuicatl <ylopez@asmedia.edu.mx>
2024-05-10 13:37:30 -0600

    Se optimizaron las imagenes y los colores de los elementos del slider

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper', {
+	slidesPerView: 5,
+	spaceBetween: 24,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
