b668bb38211b2d602afab4fc2a80a04cc1b51ca1
Yatziel Ilhuicatl <ylopez@asmedia.edu.mx>
2024-05-10 13:55:35 -0600

    Se ajustaron los settings del slider

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 var swiper = new Swiper('.swiper', {
 	slidesPerView: 5,
 	spaceBetween: 24,
+	freeMode: true,
+	loop: true,
+	lazy: true,
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
+		dynamicBullets: true,
 	},
 	navigation: {
 		nextEl: '.swiper-button-next',
